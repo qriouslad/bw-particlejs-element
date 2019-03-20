26f543e9905614d29ba134b6c74f5781fce962a7
Bowo <wibowosulistio@gmail.com>
2019-03-20 13:48:41 +0700

    Add WordPress Plugin Boilderplate files

---
+++ includes/class-bw-particlejs-element-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://bowo.io
+ * @since      1.0.0
+ *
+ * @package    Bw_Particlejs_Element
+ * @subpackage Bw_Particlejs_Element/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Bw_Particlejs_Element
+ * @subpackage Bw_Particlejs_Element/includes
+ * @author     Bowo <hello@bowo.io>
+ */
+class Bw_Particlejs_Element_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
