5ac0eb2aa17688bd3a59a87d47f21e8231961490
Bowo <wibowosulistio@gmail.com>
2019-03-21 13:09:53 +0700

    Finish shortcode feature and add explanation in the settings page.

--- admin/partials/bw-particlejs-element-admin-display.php
+++ admin/partials/bw-particlejs-element-admin-display.php
@@ -17,18 +17,40 @@
 
 <div class="wrap particlesjs-settings">
 
-	<form action="options.php" method="post">
+	<div class="layout-half">
 
-		<?php
+		<form action="options.php" method="post">
 
-		settings_fields( 'particlesjs' );
+			<?php
 
-		do_settings_sections( 'particlesjs' );
+			settings_fields( 'particlesjs' );
 
-		submit_button( 'Save Settings' );
+			do_settings_sections( 'particlesjs' );
 
-		?>
+			submit_button( 'Save Settings' );
 
-	</form>
+			?>
+
+		</form>
+
+	</div>
+
+	<div class="layout-half">
+
+		<h2>Particles.js Element via Shortcode</h2>
+
+		<p>You can <a href="https://www.competethemes.com/blog/how-to-shortcodes-wordpress/">use a shortcode</a> in a post or page, to ouput a div element with particles.js animation applied as the background. Here's an example:</p>
+
+		<div class="code-sample">
+
+			<p class="shortcode-sample">[particles <strong>minheight</strong>="200px" <strong>bgcolor</strong>="#840000" <strong>textcolor</strong>="#ffffff" <strong>heading=</strong>"Awesome Title" <strong>description</strong>="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."]</p>
+
+		</div>
+
+		<p>The <strong><em>minheight</em></strong> attribute specifies the minimum height of the element in pixels, the <strong><em>bgcolor</em></strong> attribute specifies the background color, and <strong><em>textcolor</em></strong> specifies the color of the text inside the element. Colors need to use the <a href="https://www.color-hex.com/">hex format</a>. The <strong><em>heading</em></strong> and <strong><em>description</em></strong> attributes is for the actual content that will be displayed inside the div element.</p>
+
+		<p>Note that the particle.js animation will use the JSON configuration specified on this page.</p>
+
+	</div>
 
 </div>
